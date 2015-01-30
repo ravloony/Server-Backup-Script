fff0a28b2ad12fc97f04932ba8d25aa224225424
Tom Macdonald <tm@thomas-macdonald.com>
2015-01-30 15:34:56 +0100

    Allow for multiple directories and databases.
    Also, use a proper temp directory.

--- backup.sh
+++ backup.sh
@@ -5,49 +5,69 @@
 # Copyright: Christian Beier (http://www.beier-christian.eu/) 
 # Version 1.1
 
+# Modified by Tom Macdonald (www.thomas-macdonald.com)
+# Version 1.2
 
 # Allgemeine Angaben
 MYSQL_USER=Benutzername_fuer_MySQL_meistens_root
 MYSQL_PASS=Passwort_fuer_MySQL
+MYSQL_DATABASES="database1 database2"
+
 FTP_SERVER=Adresse_des_FTP-Servers_auf_dem_gesichert_werden_soll # Bsp. Strato: backup.serverkompetenz.de
 FTP_USER=Benutzername
 FTP_PASS=Passwort
+
+DIRECTORIES="/var/www/website /etc /var/log"
 
 # Festlegung des Datums - Format: 20050710
 DATE=`date +"%Y%m%d"`
 
 # ENDE DER EINSTELLUNGEN
 
+echo "Starting backup run on $DATE"
+
 # Backup-Verzeichnis anlegen 
-mkdir /tmp/backup
-mkdir /tmp/backup/mysql
+TMPDIR=`mktemp -d /tmp/backup.XXXX`
 
-# Verzeichnisse die ins Backup integriert werden sollen
-cp -r /srv /tmp/backup
-cp -r /etc /tmp/backup
-cp -r /var/log /tmp/backup
+echo "In directory $TMPDIR"
 
-cd /tmp/backup/mysql
+cd $TMPDIR
+
+echo "Collecting databases"
 
 # Sicherung der Datenbanken
-mysqldump -AaCceQ -u$MYSQL_USER -p$MYSQL_PASS -r mysql.dbs
+for database in $MYSQL_DATABASES
+do
+	echo "Dumping $database"
+	mysqldump -u$MYSQL_USER -p$MYSQL_PASS $database | bzip2 > $database-$DATE.sql.bz2
+	echo "Done"
+done
 
-cd ../
+echo "Collecting files"
 
-# Alle Dateien mit tar.bz2 komprimieren
-tar cjfp files-$DATE.tar.bz2 srv
-tar cjfp etc-$DATE.tar.bz2 etc
-tar cjfp logs-$DATE.tar.bz2 log
-tar cjfp mysql-$DATE.tar.bz2 mysql
+for directory in $DIRECTORIES
+do
+	echo "copying $directory to $TMPDIR"
+	cp -r $directory $TMPDIR
+	dirname=`basename $directory`
+	echo "Compressing $dirname"
+	tar cjfp $dirname-$DATE.tar.bz2 $TMPDIR/$dirname
+	echo "Done"
+done
+
+echo "Starting ftp upload"
 
 # Alle komprimierten Dateien per FTP auf den Backup-Server laden
 ftp -ni << END_UPLOAD
   open $FTP_SERVER
   user $FTP_USER $FTP_PASS
   bin
-  mput *.tar.bz2
+  mput *.bz2
   quit
 END_UPLOAD
 
+echo "Removing $TMPDIR"
 # Anschliessend alle auf den Server angelegten Dateien wieder loeschen
-rm -r -f /tmp/backup
+rm -r -f $TMPDIR
+
+echo "finished"
